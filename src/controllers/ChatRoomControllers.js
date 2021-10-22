850b28f2878d63369dddc145df0e9cd44a97cafb
Vikraam27 <ramvik36@gmail.com>
2021-10-22 19:57:41 +0700

    fix bug in verify roomchat controllers

--- src/controllers/ChatRoomControllers.js
+++ src/controllers/ChatRoomControllers.js
@@ -12,7 +12,7 @@ class ChatRoomControllers {
     const query = {
       text: `SELECT chat_room.id, chat_room.creator, room_participant.participant_username FROM chat_room
       LEFT JOIN room_participant ON room_participant.room_id = chat_room.id
-      WHERE room_participant.participant_username = $1 AND chat_room.creator = $2`,
+      WHERE room_participant.participant_username = $1 AND chat_room.creator = $2 OR room_participant.participant_username = $2 AND chat_room.creator = $1`,
       values: [usernameParticipant, usernameCreator],
     };
 
